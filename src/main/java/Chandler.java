b0f3794f7f8f4e3b650b46541e296225caccd11a
your-name <your-email@example.com>
2025-08-31 23:02:33 +0800

    Add Level-1

--- src/main/java/Chandler.java
+++ src/main/java/Chandler.java
@@ -1,11 +1,34 @@
+import java.util.Scanner;
+
 public class Chandler {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello! I'm Chandler\nWhat can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm Chandler\n    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+
+        boolean isRunning = true;
+
+        while (isRunning) {
+            String input = in.nextLine();
+
+            switch (input.toLowerCase()) {
+            case "bye":
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    Bye. Hope I don't see you again!");
+                System.out.println("    ____________________________________________________________");
+                isRunning = false;
+                break;
+
+            default:
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    " + input);
+                System.out.println("    ____________________________________________________________");
+                break;
+            }
+        }
+        in.close();
     }
 }
